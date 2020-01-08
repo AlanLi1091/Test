18da4b573b1a764f09c5d203c039ea82a7fa67d5
AlanLi1091 <alanli931108@hotmail.com>
2020-01-07 17:07:12 -0800

    Project near completion.

--- rng_test2.py
+++ rng_test2.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import math
 
 rarities = ["R", "SR", "SRLimited", "SRNew", "SSR", "SSRLimited", "SSRNew"]
 weight = [float(0.85), float(0.096), float(0.012), float(0.012), float(0.0201), float(0.00495), float(0.00495)]
@@ -38,7 +39,56 @@ def turn_10r():
 
 gacha_result = []
 gacha_result_list = []
-for i in range(10):
-    gacha_result.append(turn_10r())
-    gacha_result_list += gacha_result[i]
+jewel_used = 0
+gacha_count = 0
 
+for i in range(300):
+    print("Gacha: 1 play or 10 plays")
+    user_input = input()
+    if user_input == 1:
+        gacha_result.append(turn_r())
+        gacha_result_list += gacha_result[i]
+        jewel_used += 250
+        gacha_count += 1
+        if gacha_count == 300:
+            print("You have reached maximum limit of plays allowed. Please take a card.")
+            break
+    print(gacha_result)    
+    if user_input == 10: 
+        gacha_result.append(turn_10r())
+        gacha_result_list += gacha_result[i]
+        jewel_used += 2500
+        gacha_count += 10
+        if gacha_count == 300:
+            print("You have reached maximum limit of plays allowed. Please take a card.")
+            break
+    print(gacha_result)
+
+r_counter = 0
+sr_counter = 0
+ssr_counter = 0
+ssr_limited_counter = 0
+ssr_new_counter = 0
+sr_limited_counter = 0
+sr_new_counter = 0
+
+for element in gacha_result_list:
+    if "R" in element:
+        if ("SR" not in element) and ("SSR" not in element):
+            r_counter += 1
+    if ("SR" in element) and ("SSR" not in element):
+        sr_counter += 1
+    if "SSR" in element:
+        ssr_counter += 1
+    if "SRNew" in element:
+        sr_new_counter += 1
+    if "SRLimited" in element:
+        sr_limited_counter += 1
+    if "SSRLimited" in element:
+        ssr_limited_counter += 1
+    if "SSRNew" in element:
+        ssr_new_counter += 1
+
+def prob(counter):
+    probability = float(counter / len(gacha_result_list)) * 100
+    return probability
